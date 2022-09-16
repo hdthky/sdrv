23b24e66f3c8a793ba68d4c58e09186a7c8782bd
hdthky <hdthky0@gmail.com>
2022-09-15 19:17:53 -0700

    完善scdd_ioctl()的整体框架。
    强制使用主设备号236。
    
    稍微修改代码风格。

--- scdd.c
+++ scdd.c
@@ -15,18 +15,17 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("hdthky");
 
 /*===============================================================================================*/
-static ssize_t scdd_read(struct file *filp, char __user *user_buffer, size_t count, loff_t *possition)
-{
+static ssize_t scdd_read(struct file *filp, char __user *user_buffer, size_t count, loff_t *possition) {
     printk("[scdd] Device file is read at offset = %i, read bytes count = %u\n",
             (int)*possition, (unsigned int)count);
 
-    if(*possition >= scdd_file_size)
+    if (*possition >= scdd_file_size)
         return 0;
 
-    if(*possition + count > scdd_file_size)
+    if (*possition + count > scdd_file_size)
         count = scdd_file_size - *possition;
 
-    if( copy_to_user(user_buffer, scdd_file_content + *possition, count) != 0)
+    if ( copy_to_user(user_buffer, scdd_file_content + *possition, count) != 0)
         return -EFAULT;
 
     *possition += count;
@@ -34,24 +33,34 @@ static ssize_t scdd_read(struct file *filp, char __user *user_buffer, size_t cou
 }
 
 /*===============================================================================================*/
-static ssize_t scdd_write(struct file *filp, const char __user *user_buffer, size_t count, loff_t *possition)
-{
+static ssize_t scdd_write(struct file *filp, const char __user *user_buffer, size_t count, loff_t *possition) {
     printk("[scdd] write\n");
     
     return 1;
 }
 
 /*===============================================================================================*/
-static long scdd_ioctl(struct file *filp, unsigned int cmd, unsigned long user_buffer)
-{
-    printk("[scdd] ioctl: cmd is %i\n", cmd);
+static long scdd_ioctl(struct file *filp, unsigned int cmd, unsigned long user_buffer) { // 不知为何，cmd为2时，不会进入本函数
+    int ret = 0;
+
+    printk("[scdd] ioctl: cmd is %u\n", cmd);
+
+    switch (cmd) {
+    case 3:
+        break;
+    case 4:
+        break;
+    case 5:
+        break;
+    default:
+        break;
+    }
     
-    return 0;
+    return ret;
 }
 
 /*===============================================================================================*/
-static struct file_operations simple_driver_fops = 
-{
+static struct file_operations simple_driver_fops = {
     .owner = THIS_MODULE,
     .read = scdd_read,
     .write = scdd_write,
@@ -62,30 +71,24 @@ static int device_file_major_number = 236;
 static const char device_name[] = "scd"; // simple char device
 
 /*===============================================================================================*/
-static int scdd_init(void)
-{
+static int scdd_init(void) {
     pr_info("[scdd] init: register chrdev\n");
 
-    int major_number = register_chrdev(0, device_name, &simple_driver_fops);
+    int ret = register_chrdev(device_file_major_number, device_name, &simple_driver_fops);
 
-    if(major_number < 0)
-    {
-        printk("[scdd] init: register_chrdev failed with error code %i\n", major_number);
-        return major_number;
+    if (ret < 0) {
+        device_file_major_number = ret;
+        printk("[scdd] init: register_chrdev failed with error code %i\n", ret);
+        return ret;
     }
-    
-    device_file_major_number = major_number;
 
     return 0;
 }
 
 /*===============================================================================================*/
-static void scdd_exit(void)
-{
-    pr_info("[scdd] exit: unregister chrdev\n" );
-
-    if(device_file_major_number != 0)
-    {
+static void scdd_exit(void) {   
+    if (device_file_major_number >= 0) {
+        pr_info("[scdd] exit: unregister chrdev\n" );
         unregister_chrdev(device_file_major_number, device_name);
     }
 }
