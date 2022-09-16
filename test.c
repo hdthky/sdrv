aad919ee5cc94cfd8389b3cd82d6d8c0344b6c01
hdthky <hdthky0@gmail.com>
2022-09-15 20:29:46 -0700

    add test framework

---
+++ test.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <scsi/sg.h>
+#include <sys/ioctl.h>
+#include <string.h>
+
+#define DEV_PATH "/dev/scd"
+
+int main(int argc, char const *argv[])
+{
+    int ret = EXIT_SUCCESS;
+
+    int fd = open("/dev/scd", O_RDONLY);
+
+    if(fd < 0) {
+        perror("open");
+        exit(EXIT_FAILURE);
+    }
+
+    close(fd);
+
+    return ret;
+}
