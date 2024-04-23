d9d2a19178497c355cba34e165006e3c0fa5a453
hdthky <hdthky0@gmail.com>
2024-04-23 20:17:54 +0800

    add deploy script

--- Makefile
+++ Makefile
@@ -4,17 +4,17 @@ obj-m := $(TARGET_MODULE).o
 
 # If we are running without kernel build system
 ifeq ($(KERNELRELEASE),)
-	BUILDSYSTEM_DIR:=/lib/modules/$(shell uname -r)/build
+	KDIR?=/lib/modules/$(shell uname -r)/build
 	PWD:=$(shell pwd)
 
 
 all : 
 # run kernel build system to make module
-	$(MAKE) -C $(BUILDSYSTEM_DIR) M=$(PWD) modules
+	$(MAKE) -C $(KDIR) M=$(PWD) modules
 
 clean:
 # run kernel build system to cleanup in current directory
-	$(MAKE) -C $(BUILDSYSTEM_DIR) M=$(PWD) clean
+	$(MAKE) -C $(KDIR) M=$(PWD) clean
 
 load:
 	insmod ./$(TARGET_MODULE).ko
---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+if [ "$IP" = "" ]; then
+    echo "Please provide IP address."
+    exit 1;
+fi
+
+PORT=${PORT:-22}
+REMOTE_USER=${REMOTE_USER:-root}
+DEPLOY_PATH=/tmp/sdrv
+
+ssh -p $PORT $REMOTE_USER@$IP "mkdir -p $DEPLOY_PATH" 
+if [ "$KDIR" = "" ]; then
+    scp -P $PORT sdrv.c Makefile load_dmesg.sh test_dmesg.sh $REMOTE_USER@$IP:$DEPLOY_PATH
+    ssh -p $PORT $REMOTE_USER@$IP "cd $DEPLOY_PATH; make && (make unload &>/dev/null; make load)"
+else
+    make KDIR="$KDIR" && (
+        scp -P $PORT sdrv.ko Makefile load_dmesg.sh test_dmesg.sh $REMOTE_USER@$IP:$DEPLOY_PATH
+        ssh -p $PORT $REMOTE_USER@$IP "cd $DEPLOY_PATH; make unload &>/dev/null; make load"
+    )
+fi
