246af3d664652e5a5df8aca8af3cecf9826fe4b4
hdthky <hdthky0@gmail.com>
2024-04-24 10:13:44 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,13 @@ REMOTE_USER=${REMOTE_USER:-root}
 DEPLOY_PATH=/tmp/sdrv
 
 ssh -p $PORT $REMOTE_USER@$IP "mkdir -p $DEPLOY_PATH" 
-scp -P $PORT sdrv.c Makefile load_dmesg.sh test_dmesg.sh $REMOTE_USER@$IP:$DEPLOY_PATH
-ssh -p $PORT $REMOTE_USER@$IP "cd $DEPLOY_PATH; make && (make unload; make load)"
+if [ "$BUILD_LOCAL" = 1 ]; then
+    if [ "$KDIR" = "" ]; then echo "Please provide kernal directory."; exit 1; fi
+    make KDIR="$KDIR" && (
+        scp -P $PORT sdrv.ko Makefile load_dmesg.sh test_dmesg.sh $REMOTE_USER@$IP:$DEPLOY_PATH
+        ssh -p $PORT $REMOTE_USER@$IP "cd $DEPLOY_PATH; make unload &>/dev/null; make load"
+    )
+else
+    scp -P $PORT sdrv.c Makefile load_dmesg.sh test_dmesg.sh $REMOTE_USER@$IP:$DEPLOY_PATH
+    ssh -p $PORT $REMOTE_USER@$IP "cd $DEPLOY_PATH; make && (make unload &>/dev/null; make load)"
+fi
