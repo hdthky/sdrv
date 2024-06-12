13bb08c66accd393c77f87a56a83068bba861c9a
hdthky <hdthky0@gmail.com>
2024-06-12 13:25:07 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -11,11 +11,11 @@ DEPLOY_PATH=/tmp/sdrv
 
 ssh -p $PORT $REMOTE_USER@$IP "mkdir -p $DEPLOY_PATH" 
 if [ "$KDIR" = "" ]; then
-    scp -P $PORT sdrv.c Makefile load_dmesg.sh test_dmesg.sh $REMOTE_USER@$IP:$DEPLOY_PATH
+    scp -P $PORT *.c Makefile load_dmesg.sh test_dmesg.sh $REMOTE_USER@$IP:$DEPLOY_PATH
     ssh -p $PORT $REMOTE_USER@$IP "cd $DEPLOY_PATH; make && (make unload &>/dev/null; make load)"
 else
     make KDIR="$KDIR" && (
-        scp -P $PORT sdrv.ko Makefile load_dmesg.sh test_dmesg.sh $REMOTE_USER@$IP:$DEPLOY_PATH
+        scp -P $PORT *.ko Makefile load_dmesg.sh test_dmesg.sh $REMOTE_USER@$IP:$DEPLOY_PATH
         ssh -p $PORT $REMOTE_USER@$IP "cd $DEPLOY_PATH; make unload &>/dev/null; make load"
     )
 fi
