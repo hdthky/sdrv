eeb0677db2d6c72e55d3f0eb6b6b68445db6ded6
hdthky <hdthky0@gmail.com>
2024-06-12 13:44:01 +0800

    update

--- scdd.c
+++ scdd.c
@@ -1,13 +1,13 @@
 #define DEBUG
 
-#include <linux/init.h>       /* module_init, module_exit */
-#include <linux/module.h>     /* version info, MODULE_LICENSE, MODULE_AUTHOR, printk() */
-#include <linux/fs.h> 	      /* file stuff */
-#include <linux/kernel.h>     /* printk() */
-#include <linux/errno.h>      /* error codes */
-#include <linux/module.h>     /* THIS_MODULE */
-#include <linux/cdev.h>       /* char device stuff */
-#include <linux/uaccess.h>    /* copy_to_user() */
+#include <linux/cdev.h>    /* char device stuff */
+#include <linux/errno.h>   /* error codes */
+#include <linux/fs.h>      /* file stuff */
+#include <linux/init.h>    /* module_init, module_exit */
+#include <linux/kernel.h>  /* printk() */
+#include <linux/module.h>  /* version info, MODULE_LICENSE, MODULE_AUTHOR, printk() */
+#include <linux/module.h>  /* THIS_MODULE */
+#include <linux/uaccess.h> /* copy_to_user() */
 
 static const char scdd_file_content[] = "scdd is simple char device driver\0";
 static const ssize_t scdd_file_size = sizeof(scdd_file_content);
@@ -19,7 +19,7 @@ MODULE_AUTHOR("hdthky");
 /*===============================================================================================*/
 static ssize_t scdd_read(struct file *filp, char __user *user_buffer, size_t count, loff_t *possition) {
     pr_debug("[scdd] Device file is read at offset = %i, read bytes count = %u\n",
-            (int)*possition, (unsigned int)count);
+             (int)*possition, (unsigned int)count);
 
     if (*possition >= scdd_file_size)
         return 0;
@@ -27,7 +27,7 @@ static ssize_t scdd_read(struct file *filp, char __user *user_buffer, size_t cou
     if (*possition + count > scdd_file_size)
         count = scdd_file_size - *possition;
 
-    if ( copy_to_user(user_buffer, scdd_file_content + *possition, count) != 0)
+    if (copy_to_user(user_buffer, scdd_file_content + *possition, count) != 0)
         return -EFAULT;
 
     *possition += count;
@@ -37,27 +37,27 @@ static ssize_t scdd_read(struct file *filp, char __user *user_buffer, size_t cou
 /*===============================================================================================*/
 static ssize_t scdd_write(struct file *filp, const char __user *user_buffer, size_t count, loff_t *possition) {
     pr_debug("[scdd] write\n");
-    
+
     return 1;
 }
 
 /*===============================================================================================*/
-static long scdd_ioctl(struct file *filp, unsigned int cmd, unsigned long user_buffer) { // 不知为何，cmd为2时，不会进入本函数
+static long scdd_ioctl(struct file *filp, unsigned int cmd, unsigned long user_buffer) {  // 不知为何，cmd为2时，不会进入本函数
     int ret = 0;
 
     pr_debug("[scdd] ioctl: cmd is %u\n", cmd);
 
     switch (cmd) {
-    case 3:
-        break;
-    case 4:
-        break;
-    case 5:
-        break;
-    default:
-        break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        default:
+            break;
     }
-    
+
     return ret;
 }
 
@@ -94,7 +94,7 @@ static struct file_operations simple_driver_fops = {
 };
 
 static int device_file_major_number = 236;
-static const char device_name[] = "scd"; // simple char device
+static const char device_name[] = "scd";  // simple char device
 
 /*===============================================================================================*/
 static int scdd_init(void) {
@@ -114,9 +114,9 @@ static int scdd_init(void) {
 }
 
 /*===============================================================================================*/
-static void scdd_exit(void) {   
+static void scdd_exit(void) {
     if (device_file_major_number >= 0) {
-        pr_info("[scdd] exit: unregister chrdev\n" );
+        pr_info("[scdd] exit: unregister chrdev\n");
         unregister_chrdev(device_file_major_number, device_name);
     }
 }
